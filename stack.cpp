ee980a1b345ff4bbdbba7e7238016a4c8e8c2232
Lavish <lavishkalra002@gmail.com>
2025-04-04 16:16:09 +0530

    Hello

---
+++ stack.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int stack[100],n=100,top=-1;
+void push(int x)
+{
+    if(top>=n-1){
+        cout<<"Stack Overflow\n";
+    }
+    else
+    {
+        top++;
+        stack[top]=x;
+    }
+}
+void pop()
+{
+    if(top==-1){
+        cout<<"Stack Underflow\n";
+    }
+    else{
+        top--;
+    }
+}
+void display(){
+    if(top==-1){
+        cout<<"Stack is empty\n";
+    }
+    else{
+        for(int i=top;i>=0;i--){
+            cout<<stack[i]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+    pop();
+    display();
+    pop();
+    display();
+    pop();
+    return 0;
+}
