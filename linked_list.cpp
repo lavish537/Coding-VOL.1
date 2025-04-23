0efe3c1be66de1451a18da0f67b138bbedfa80df
Lavish <lavishkalra002@gmail.com>
2025-04-24 04:32:00 +0530

    Refactor linked_list.cpp to improve function naming and consistency in node insertion methods

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,11 +9,46 @@ struct Node{
 
 struct Node *head=NULL;
 
-void insert_Node(int n){
+void insert_at_head_Node(int n){
     struct Node *new_node=new Node;
     new_node->num=n;
     new_node->next=head;
     head=new_node;
+}
+
+void insert_at_tail_Node(int n){
+    struct Node *new_node=new Node;
+    new_node->num=n;
+    new_node->next=NULL;
+    if(head==NULL){
+        head=new_node;
+        return;
+    }
+    struct Node *temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=new_node;
+}
+
+void insert_at_position_Node(int n,int pos){
+    struct Node *new_node=new Node;
+    new_node->num=n;
+    if(pos==0){
+        new_node->next=head;
+        head=new_node;
+        return;
+    }
+    struct Node *temp=head;
+    for(int i=0;i<pos-1;i++){
+        if(temp==NULL){
+            cout<<"Position out of bounds\n";
+            return;
+        }
+        temp=temp->next;
+    }
+    new_node->next=temp->next;
+    temp->next=new_node;
 }
 
 void display_all_nodes(){
@@ -27,12 +62,12 @@ void display_all_nodes(){
 }
 
 int main(){
-    insert_Node(1);
-    insert_Node(3);
-    insert_Node(5);
-    insert_Node(7);
-    insert_Node(9);
-    insert_Node(11);
+    insert_at_tail_Node(1);
+    insert_at_tail_Node(3);
+    insert_at_tail_Node(5);
+    insert_at_tail_Node(7);
+    insert_at_tail_Node(9);
+    insert_at_tail_Node(11);
     display_all_nodes();
     return 0;
 }
